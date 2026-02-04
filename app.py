d0799e44f7c032a2286873aa8406e78c7e6752b6
Anthony <amedassi@lasalle63.fr>
2026-02-04 15:26:45 +0100

    Stockage sha256 OK

---
+++ app.py
@@ -0,0 +1,131 @@
+from flask import Flask, request, render_template, redirect, session, g
+import sqlite3 , hashlib
+
+app = Flask(__name__)
+app.secret_key = "secret_key_dev" 
+DB_NAME = "users.db"
+
+def hacherUnMotDePasse(motDePasseEnClair):
+    mdpHashe = hashlib.sha256(motDePasseEnClair.encode()).hexdigest()
+    return mdpHashe
+
+
+# -----------------------------
+# Gestion de la base de données
+# -----------------------------
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DB_NAME)
+    return db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+# -----------------------------
+# Initialisation de la base
+# -----------------------------
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE,
+            password TEXT,
+            nom TEXT,
+            prenom TEXT,
+            email TEXT,
+            avatar TEXT) """)
+    cursor.execute("""
+        INSERT OR IGNORE INTO users 
+        (username, password, nom, prenom, email, avatar)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """, (
+            "admin",
+            hacherUnMotDePasse("toto"),
+            "Dupont",
+            "Alice",
+            "alice.dupont@example.com",
+            "https://api.dicebear.com/9.x/adventurer/svg?seed=admin"))
+    conn.commit()
+    conn.close()
+
+# -----------------------------
+# Connexion
+# -----------------------------
+@app.route("/", methods=["GET", "POST"])
+def login():
+    message = ""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?",(username, hacherUnMotDePasse(password)))
+        user = cursor.fetchone()
+        if user:
+            session["username"] = username
+            return redirect("/home")
+        else:
+            message = "Identifiants incorrects."
+    return render_template("login.html", message=message)
+
+# -----------------------------
+# Home
+# -----------------------------
+@app.route("/home")
+def home():
+    if "username" not in session:
+        return redirect("/")
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT nom, prenom, email, avatar
+        FROM users
+        WHERE username = ?
+        """, (session["username"],))
+    user = cursor.fetchone()
+    return render_template(
+        "home.html",
+        username=session["username"],
+        nom=user[0],
+        prenom=user[1],
+        email=user[2],
+        avatar=user[3])
+
+# -----------------------------
+# Changement de mot de passe
+# -----------------------------
+@app.route("/change-password", methods=["GET", "POST"])
+def change_password():
+    if "username" not in session:
+        return redirect("/")
+    message = ""
+    username = session["username"]
+    if request.method == "POST":
+        new_password = request.form["new_password"]
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("UPDATE users SET password = ? WHERE username = ?",(hacherUnMotDePasse(new_password), username))
+        db.commit()
+        message = "Mot de passe modifié."
+    return render_template("change_password.html",  username=username, message=message)
+
+# -----------------------------
+# Déconnexion
+# -----------------------------
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+# -----------------------------
+# Lancement
+# -----------------------------
+if __name__ == "__main__":
+    init_db()
+    app.run(host="0.0.0.0", port=5000, debug=True)
